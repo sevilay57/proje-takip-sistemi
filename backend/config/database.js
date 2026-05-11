e03225f252fa65935e4a7c698675d073a8043bbe
Sevilay <sevilaydn5734@gmail.com>
2026-05-12 00:03:05 +0300

    postgresql gecis

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,15 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "./database.sqlite",
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  logging: false,
 });
 
 module.exports = sequelize;
